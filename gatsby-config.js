5f6e4ad23d4c6d39a0a859ad6ca112939753dfa8
Gerly De Ceuster <gerly.deceuster@student.ap.be>
2020-11-16 13:02:34 +0100

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,14 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
+      resolve: "gatsby-source-graphql",
+      options: {
+        typeName: "WPGraphql",
+        fieldName: "wpcontent",
+        url: "http://artist-agency.codobi.be/graphql",
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
